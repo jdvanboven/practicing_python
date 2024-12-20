922fa07d030ccb7a3b53ff599305f10620d36e48
jdvanboven <79928025+jdvanboven@users.noreply.github.com>
2024-12-20 12:44:57 +0100

    Add files via upload

---
+++ connect-four-game.py
@@ -0,0 +1,30 @@
+# The piece_list keeps track of which pieces have been placed
+piece_list = [["-","-","-","-","-","-","-"],
+              ["-","-","-","-","-","-","-"],
+              ["-","-","-","-","-","-","-"],
+              ["-","-","-","-","-","-","-"],
+              ["-","-","-","-","-","-","-"],
+              ["-","-","-","-","-","-","-"]]
+
+
+def display_game(symbol, grid_width, grid_height):
+    c = 65
+
+    # First row
+    print(f"  ", end='')
+    for j in range(grid_width):
+        print(f"| {j+1} ", end='')
+    print("| ")
+    print((grid_width*4+4)*"-")
+
+    # Other rows
+    for i in range(grid_height):
+        print(f"{chr(c+i)} ", end='')
+        for j in range(grid_width):
+            print(f"| {symbol} ", end='')
+        print("| ")
+        print((grid_width*4+4)*"-")
+
+
+display_game('~', 7, 6)
+
