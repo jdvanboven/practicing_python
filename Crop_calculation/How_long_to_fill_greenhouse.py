3c941a95b8dd1918b761a241928932a3186994ab
Joram van Boven <joram.van.boven@rivm.nl>
2025-04-22 14:50:22 +0200

    Added new file for crop calculation

---
+++ Crop_calculation/How_long_to_fill_greenhouse.py
@@ -0,0 +1,28 @@
+'''
+This script can be used to calculate how long it takes to fill the greenhouse with a certain crop.
+It needs to know how long a crop takes to mature, how many products it produces and if it produces once or more times.
+It assumes the products are put into a seed maker and that the seed maker produces X seeds on average.
+'''
+
+
+class Plant:
+    def __init__(self)
+        self.age = 0
+
+    def increase_age(self)
+        self.age += 7
+
+def age_plants():
+    pass
+
+def plant_new_seeds():
+    pass
+
+def main()
+    while greenhouse_full == False:
+        age_plants()
+        plant_new_seeds()
+
+
+if __name__ == "__main__":
+    main()
